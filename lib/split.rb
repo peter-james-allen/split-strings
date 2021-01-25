180443f23f01086bad0a25292da381a820653180
Pete <peter_james_allen@hotmail.com>
2021-01-25 18:08:34 +0000

    third test passing

--- lib/split.rb
+++ lib/split.rb
@@ -1,6 +1,8 @@
 def split(str)
     if str.length == 1
         [str + '_']
+    elsif str.length > 2
+        [str[0..1], str[2..str.length]]
     else
         [str]
     end
--- spec/split_spec.rb
+++ spec/split_spec.rb
@@ -7,4 +7,7 @@ describe 'Split-Stings' do
     it 'test sting of length 1' do
         expect(split('a')).to eq(['a_'])
     end
+    it 'test sting of length 4' do
+        expect(split('abcd')).to eq(['ab','cd'])
+    end
 end
